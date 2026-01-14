b781b16d35efa806244651534852da9938d0e78a
Beamu <prebeamu@gmail.com>
2026-01-14 14:00:38 +0700

    FuN oOp

---
+++ Object Oriented Programming (OOP)/PreMidterm/src/Pigeous.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author prebe
+ */
+public class Pigeous extends Bird {
+
+    public Pigeous() {
+        super("", 0);
+        this.setPower(150);
+    }
+
+    public Pigeous(String name, int age) {
+        super(name, age);
+        this.setPower(150);
+    }
+    
+    @Override
+    public void eat(Food f) {
+        this.setPower(this.getPower() + (f.getPower()*2));
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(this.getName() + " fly fly ....");
+    }
+
+    @Override
+    public void wingAttack(Animal a) {
+        a.setPower(a.getPower() - 5);
+    }
+
+    public void wingAttack(Animal a, int times) {
+        a.setPower(a.getPower() - (5*times));
+    }
+}
